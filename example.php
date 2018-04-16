fc9889827a4a4d35c1ab514b27e5b04c0c815eac
wojciechandruszkiewicz <wojciech.andruszkiewicz@shoplo.com>
2018-04-16 11:26:24 +0200

    store client

--- example.php
+++ example.php
@@ -5,38 +5,22 @@ require_once __DIR__.'/vendor/autoload.php';
 ini_set('display_errors', 'TRUE');
 error_reporting(E_ALL);
 
-define('SECRET_KEY', '89da2c8cad0f74a558bc9c47924dc9e6');
-define('PUBLIC_KEY', '95897d01c802ed5ceae1348bb0efae7a');
+define('SECRET_KEY', 'xxx');
+define('PUBLIC_KEY', 'yyy');
 
 define('CALLBACK_URL', 'http://127.0.0.1/store-client/example.php');
 
 $accessToken = $refreshToken = null;
 
 $config = [
-    'apiBaseUrl' => 'http://api.s6.shoplo.tech',
-    'authBaseUrl' => 'http://auth.s6.test.shoplo.io',
+    'apiBaseUrl' => 'http://api.shoplo.io',
+    'authBaseUrl' => 'http://auth.shoplo.io',
     'publicKey' => PUBLIC_KEY,
     'secretKey' => SECRET_KEY,
     'callbackUrl' => CALLBACK_URL,
     'accessToken' => $accessToken,
     'refreshToken' => $refreshToken,
 ];
-
-//$guzzleConfig = [
-//    'base_uri' => 'http://auth.s6.test.shoplo.io',
-//];
-
-//$guzzleAdapter = new \SSOAuth\Guzzle\GuzzleAdapter(
-//    new \GuzzleHttp\Client($guzzleConfig)
-//);
-//$shoploMultiClient = new \SSOAuth\SSOAuthClient(
-//    $guzzleAdapter,
-//    $config
-//);
-//
-//$response = $shoploMultiClient->authorize();
-//echo $shoploMultiClient->ssoAppId;
-//print_r($response);
 
 $shoploApi = new Shoplo\ShoploApi($config);
 $shoploApi->initSSOAuthClient($config);
